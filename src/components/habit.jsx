3000632ba7cacc09e1c24454fe4faacbe9f8d21f
handsm214bj <63544016+handsm214bj@users.noreply.github.com>
2020-12-25 19:49:14 +0900

    add habit-tracker

---
+++ src/components/habit.jsx
@@ -0,0 +1,52 @@
+import React, { PureComponent } from 'react';
+
+class Habit extends PureComponent {
+    componentDidMount() {
+        console.log(`habit: ${this.props.habit.name} mounted`);
+    }    
+
+    componentWillUmount() {
+        console.log(`habit: ${this.props.habit.name} will unmount`);
+    }
+
+    handleIncrement = () => {
+        this.props.onIncrement(this.props.habit);
+        };
+
+    handleDecrement = () => {
+        this.props.onDecrement(this.props.habit);
+        };
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
+
+    render() {
+        const { name, count } = this.props.habit;
+        return (
+        <li className="habit">
+         <span className="habit-name">{name}</span>
+         <span className="habit-count">{count}</span>
+         <button 
+            className="habit-button habit-increase"
+            onClick={this.handleIncrement}
+        >
+            <i class="fas fa-plus-square"></i>
+         </button>
+         <button 
+         className="habit-button habit-decrease" 
+         onClick={this.handleDecrement}
+         >
+            <i class="fas fa-minus-square"></i>   
+         </button>
+         <button className="habit-button habit-delete"
+         onClick={this.handleDelete}
+         >
+         <i class="fas fa-trash"></i>    
+         </button>
+        </li>
+        );
+    } class
+}
+
+export default Habit;
---
+++ src/components/habitAddform.jsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react';
+
+const HabitAddform = memo(props => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();    
+        
+    const onSubmit = event => {
+        event.preventDefault();
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        formRef.current.reset();
+    };    
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+        <input 
+            ref={inputRef}
+            type="text" 
+            className="add-input" 
+            placeholder="Habit"
+        />
+        <button className="add-button">명단추가</button>
+        </form>
+    );
+});
+
+export default HabitAddform;
+
